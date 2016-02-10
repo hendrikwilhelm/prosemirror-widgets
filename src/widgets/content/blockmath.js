34c0df320a0531a4d2f7c673c5424b09ba39e102
Hendrik Wilhelm <hendrik.wilhelm@researchgate.net>
2016-02-10 10:10:43 +0000

    mathjax import, bundle update

--- src/widgets/content/blockmath.js
+++ src/widgets/content/blockmath.js
@@ -1,7 +1,8 @@
 import {Block, Attribute} from "prosemirror/dist/model"
 import {elt,insertCSS} from "prosemirror/dist/dom"
 import {defParser, defParamsClick, selectedNodeAttr} from "../../utils"
- 
+import 'mathjax';
+
 export class BlockMath extends Block {
 	get attrs() {
 		return {
@@ -21,13 +22,13 @@ BlockMath.prototype.serializeDOM = node => {
 		// wait until node is attached to document to render
 		MathJax.Hub.Queue(["Delay",MathJax.Callback,100],["Typeset",MathJax.Hub,node.rendered])
 	}
-	return node.rendered; 
+	return node.rendered;
 }
 
 BlockMath.register("command", "insert", {
 	derive: {
 		params: [
-	      	{ name: "Latex", attr: "tex", label: "Latex Expression", type: "text", 
+	      	{ name: "Latex", attr: "tex", label: "Latex Expression", type: "text",
 	      	  prefill: function(pm) { return selectedNodeAttr(pm, this, "tex") }}
 	 	]
     },
