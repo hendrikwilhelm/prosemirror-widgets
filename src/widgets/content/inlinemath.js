5ea443f23e83936f01bcf876948c72412b6414f0
Hendrik Wilhelm <hendrik.wilhelm@researchgate.net>
2016-02-10 14:42:51 +0000

    mathjax dependencies in bundle

--- src/widgets/content/inlinemath.js
+++ src/widgets/content/inlinemath.js
@@ -5,6 +5,9 @@ import {defParser, defParamsClick, selectedNodeAttr} from "../../utils"
 require('mathjax');
 require('mathjax/extensions/MathMenu');
 require('mathjax/extensions/MathZoom');
+require('mathjax/config/TeX-AMS-MML_HTMLorMML');
+require('mathjax/jax/output/HTML-CSS/jax');
+require('mathjax/jax/output/HTML-CSS/fonts/STIX/fontdata');
 
 export class InlineMath extends Inline {
 	get attrs() {
